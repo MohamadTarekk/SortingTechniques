f27cf70af0f6866fe7b21d9a8f2609750b63a495
MohamadTarekk <mohamadtareksh@gmail.com>
2019-03-23 17:18:27 +0200

    GUI + Plotting

--- src/controller/ThePrestige.java
+++ src/controller/ThePrestige.java
@@ -1,54 +1,32 @@
 package controller;
 
-//import view.*;
-import model.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class ThePrestige {
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-	public static void main(String[] args) {
-		
-		// testing heap sort
-		/*
-		int[] x = {1,2,3,5,4,6,8,-52,52,4516,622,74,12,65};
-		HeapSort myHeap = new HeapSort(x);
-		myHeap.sort();
-		myHeap.printArray();
-		myHeap.printHeap();
-		myHeap.sort();
-		myHeap.printArray();
-		myHeap.printHeap();
-		*/
-		
-		// testing insertion sort
-		/*
-		int[] y = {2,20,4,3,30};
-        InsertionSort list = new InsertionSort(y); 
-        list.push(5); 
-        list.push(20); 
-        list.push(4); 
-        list.push(3); 
-        list.push(30); 
-        System.out.println("Linked List before Sorting.."); 
-        list.printlist(list.getHead()); 
-        list.sort(list.getHead()); 
-        System.out.println("\nLinkedList After sorting"); 
-        list.printlist(list.getHead()); 
-		*/
-		
-        // testing selection sort
-    	/*
-    	int arr[] = {64,25,12,22,11}; 
-        SelectionSort ob = new SelectionSort(arr); 
-        ob.sort(); 
-        System.out.println("Sorted array"); 
-        ob.printArray(); 
-        */
-		
-        // testing quick sort
-        int[] q = {10, 7, 8, 9, 1, 5};
-        QuickSort quickSort = new QuickSort(q); 
-        quickSort.sort();
-        System.out.println("sorted array"); 
-        quickSort.printArray();
-	}
+//import model.*;
+//import model.*;
+
+public class ThePrestige extends Application {
+
+	public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Path currentRelativePath = Paths.get("");
+        String path = currentRelativePath.toAbsolutePath().toString() + "\\src\\view\\TheStage.fxml";
+        Parent root = FXMLLoader.load(new File(path).toURI().toURL());
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 1300, 800));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
 }
--- src/view/Stage.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-public class Stage {
-    /*
-    * This commment is to test possible conflicts in committing and pushing to github
-    * */
-}
